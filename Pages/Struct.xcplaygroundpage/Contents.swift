4cca0fd5c4ff0b8799fb4328feecc2909625a1ea
haejii <yanghagi@naver.com>
2021-09-08 21:24:02 +0900

    Practice Struct

--- Pages/Struct.xcplaygroundpage/Contents.swift
+++ Pages/Struct.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
 //: [Previous](@previous)
+import UIKit
 
-import Foundation
+struct User{
+    var nickname:String
+    var age: Int
+    
+    func information(){
+        print("\(nickname) \(age)")
+    }
+}
 
-var greeting = "Hello, playground"
+// 구조체 사용하기 위해 인스턴스 생성
 
+var user = User(nickname: "haeji", age: 24)
+
+user.nickname
+user.nickname = "albert"
+user.nickname
+
+user.information()
 //: [Next](@next)
