1b3911d19bf760ad47a23766e0c98b18866e5440
haejii <yanghagi@naver.com>
2021-09-08 21:26:57 +0900

    Practice Class

--- Pages/Class.xcplaygroundpage/Contents.swift
+++ Pages/Class.xcplaygroundpage/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Dog{
+    var name:String = ""
+    var age: Int = 0
+    
+    init(){
+        
+    }
+    
+    func introduce() {
+        print("name \(name) age \(age)")
+    }
+}
+
+var dog = Dog()
+dog.name = "CoCo"
+dog.age = 3
+dog.name
+dog.age
+
+dog.introduce()
